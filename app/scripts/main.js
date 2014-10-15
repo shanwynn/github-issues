b56f2e7a5407b97619e533abbd9b6af25611fe03
Shannon Wynn <shannonwynn@mac.com>
2014-10-14 21:34:01 -0400

    working on it

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,30 @@
-console.log('The Iron Yard Rocks');
+/*console.log('Testing API');*/
+
+/*var APIping = $.ajax({url:'https://api.github.com/repos/shanwynn/my-proj/issues'}).done(function(e) { return e });*/
+
+var hitMe = $('button');
+hitMe.on('click'), function(e) {
+  getIssues();
+};
+
+var getIssues = function () {
+  var token = $('#token').val();
+  var APIurl = $('#url').val();
+};
+
+var iContainer = $('#issues');
+var iTemplate = _.template($('#iTemplate').html());
+var iState = _.template($('#iState').html());
+var iNumber = _.template($('#iNumber').html());
+
+$.ajax({ url: APIurl }).done(function (data) {
+  data.forEach(function(issue) {
+    iContainer.append(iTemplate(issue));
+  });
+});
+
+setInterval(function() {
+  $.ajax({ url: APIurl }).done(function(data) {
+    $("#iCount").text(data.length);
+  });
+}, 1000);
